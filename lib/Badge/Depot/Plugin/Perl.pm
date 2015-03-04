cb5ed813eef19545f2754cecbfa87dbeabe52c88
Erik Carlsson <info@code301.com>
2015-03-05 00:16:16 +0100

    Init

---
+++ lib/Badge/Depot/Plugin/Perl.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Badge::Depot::Plugin::Perl;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Badge::Depot::Plugin::Perl;
+
+=head1 DESCRIPTION
+
+Badge::Depot::Plugin::Perl is ...
+
+=head1 SEE ALSO
+
+=cut
